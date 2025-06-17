01cd76294a5e50fc3c24ef9c6cd57a074bf4b94d
Marija796 <121623159+Marija796@users.noreply.github.com>
2025-06-17 23:33:23 +0200

    Fix build script for Vercel

--- frontend/Task-Manager/vite.config.js
+++ frontend/Task-Manager/vite.config.js
@@ -2,13 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: './', // ← ова е клучно за Vercel!
+  base: './', // <-- ова е клучно!
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
       '/api': 'http://localhost:8000'
     }
   }
-});
+})
